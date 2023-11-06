6a33969f400277a2403b5303e7324405bf50d755
SonSeungGyu <thstmd0705@naver.com>
2023-11-06 13:48:45 +0900

    퀴즈 Avante 클래스에서 각 메소드기능 수정

--- src/abstractex/quiz/Quiz1.java
+++ src/abstractex/quiz/Quiz1.java
@@ -41,19 +41,19 @@ class Avante extends Car{
 
 	@Override
 	public void drive() {
-		System.out.println("Avante 달립니다.");
+		System.out.println(name+" 달립니다.");
 		
 	}
 
 	@Override
 	public void stop() {
-		System.out.println("Avante 멈춥니다.");
+		System.out.println(name+" 멈춥니다.");
 		
 	}
 
 	@Override
 	public void turnOff() {
-		System.out.println("Avante 시동을 끕니다.");
+		System.out.println(name+" 시동을 끕니다.");
 		
 	}
 	
